ff5220d384fcbe5921444d2eba6748f31ad4dafb
Gergo Bogdan <bogdangergo@gmail.com>
2016-12-30 14:11:13 +0100

    added sample code

--- index.js
+++ index.js
@@ -15,16 +15,26 @@ console.log('Hello Functional Programming in JS');
 
 //console.log(funcPrime.isPrime(funcPrime.mersenneNumber(31)));
 
-var x = [1,2,3];
-var res = [];
-var f = function(p) { return p * 2; }
+var x = [1,2,3,4,5];
+var y = [6,7,8,'a',9,10,'b',11];
+var double = function(p) { return p * 2; }
 
-funcHelper.forEach(x, function(item) {
-    res.push(item * 2);
-});
+console.log(funcHelper.applyIfAllNumbers(x, double));
+console.log(funcHelper.applyIfAllNumbers(y, double));
 
-console.log(res);
-console.log(funcHelper.map([1,2,3,4], function(x) { return 2*x;}));
+console.log(funcHelper.applyForNumbersOnly(y, double));
 
-
-console.log(funcPrime.isPrime(7));
+// var res = [];
+//
+//
+// funcHelper.forEach(x, function(item) {
+//     res.push(item * 2);
+// });
+//
+// console.log(res);
+// console.log(funcHelper.map([1,2,3,4], function(x) { return 2*x;}));
+//
+//
+// console.log(funcPrime.isPrime(7));
+//
+// console.log(funcHelper.filter(x, function(nr) { return nr * 2 > 3;}))
